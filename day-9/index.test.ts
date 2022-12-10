5196f3161d3634c504e33992364c960d196ffdf3
Michal Michalczuk <michalczukm@gmail.com>
2022-12-10 03:34:18 +0100

    day 9, part 1, attempt 1

--- day-9/index.test.ts
+++ day-9/index.test.ts
@@ -1,6 +1,6 @@
 import { readFile } from 'node:fs/promises';
 import { join } from 'node:path';
-import { runPartOne, runPartTwo } from '.';
+import { calculateTailPosition, runPartOne, runPartTwo } from '.';
 
 describe('day 9', () => {
   it('part one should work for sample input', async () => {
@@ -8,7 +8,7 @@ describe('day 9', () => {
       await readFile(join(__dirname, './sample-input'))
     ).toString();
 
-    expect(runPartOne(sampleInput)).toBe(95437);
+    expect(runPartOne(sampleInput)).toBe(13);
   });
 
   it('part two should work for sample input', async () => {
@@ -17,5 +17,61 @@ describe('day 9', () => {
     ).toString();
 
     expect(runPartTwo(sampleInput)).toBe(24933642);
+  });
+  describe('calculateTailPosition', () => {
+    it('start', () => {
+      const actual = calculateTailPosition({ x: 0, y: 0 }, { x: 0, y: 0 });
+      expect(actual).toEqual({ x: 0, y: 0 });
+    });
+
+    it('right hold', () => {
+      const actual = calculateTailPosition({ x: 0, y: 0 }, { x: 1, y: 0 });
+      expect(actual).toEqual({ x: 0, y: 0 });
+    });
+
+    it('right', () => {
+      const actual = calculateTailPosition({ x: 0, y: 0 }, { x: 2, y: 0 });
+      expect(actual).toEqual({ x: 1, y: 0 });
+    });
+
+    it('left', () => {
+      const actual = calculateTailPosition({ x: 3, y: 3 }, { x: 1, y: 3 });
+      expect(actual).toEqual({ x: 2, y: 3 });
+    });
+
+    it('top', () => {
+      const actual = calculateTailPosition({ x: 3, y: 3 }, { x: 3, y: 5 });
+      expect(actual).toEqual({ x: 3, y: 4 });
+    });
+
+    it('down', () => {
+      const actual = calculateTailPosition({ x: 3, y: 3 }, { x: 3, y: 1 });
+      expect(actual).toEqual({ x: 3, y: 2 });
+    });
+
+    it('diagonal hold', () => {
+      const actual = calculateTailPosition({ x: 0, y: 0 }, { x: 1, y: 1 });
+      expect(actual).toEqual({ x: 0, y: 0 });
+    });
+
+    it('diagonal top right', () => {
+      const actual = calculateTailPosition({ x: 0, y: 0 }, { x: 1, y: 2 });
+      expect(actual).toEqual({ x: 1, y: 1 });
+    });
+
+    it('diagonal top left', () => {
+      const actual = calculateTailPosition({ x: 3, y: 1 }, { x: 2, y: 3 });
+      expect(actual).toEqual({ x: 2, y: 2 });
+    });
+
+    it('diagonal down left', () => {
+      const actual = calculateTailPosition({ x: 4, y: 5 }, { x: 3, y: 3 });
+      expect(actual).toEqual({ x: 3, y: 4 });
+    });
+
+    it('diagonal down right', () => {
+      const actual = calculateTailPosition({ x: 2, y: 5 }, { x: 3, y: 3 });
+      expect(actual).toEqual({ x: 3, y: 4 });
+    });
   });
 });
--- day-9/index.ts
+++ day-9/index.ts
@@ -1,5 +1,5 @@
 type Move = {
-  direction: 'L' | 'R' | 'U' | 'D';
+  direction: 'L' | 'R' | 'U' | 'D' | 'LD' | 'LU' | 'RD' | 'RU';
 };
 
 type Point = { x: number; y: number };
@@ -29,19 +29,74 @@ const GAME_MAP: GameHistory = [
   },
 ];
 
-const calculateNextPoint = (move: Move, previous: Point): Point => {
+const calculateNextPoint = (
+  direction: Move['direction'],
+  previous: Point
+): Point => {
   const moveOperations = {
     L: { x: previous.x - 1, y: previous.y },
     R: { x: previous.x + 1, y: previous.y },
     U: { x: previous.x, y: previous.y + 1 },
     D: { x: previous.x, y: previous.y - 1 },
+    LD: { x: previous.x - 1, y: previous.y - 1 },
+    LU: { x: previous.x - 1, y: previous.y + 1 },
+    RD: { x: previous.x + 1, y: previous.y - 1 },
+    RU: { x: previous.x + 1, y: previous.y + 1 },
   } as const;
 
-  return moveOperations[move.direction];
+  return moveOperations[direction];
 };
 
-const calculateTailPosition = (previousTail: Point, newHead: Point): Point => {
-  // FOR NOW
+export const calculateTailPosition = (
+  previousTail: Point,
+  newHead: Point
+): Point => {
+  const distance = Math.floor(
+    Math.sqrt(
+      Math.pow(previousTail.x - newHead.x, 2) +
+        Math.pow(previousTail.y - newHead.y, 2)
+    )
+  );
+
+  if (distance <= 1) {
+    return previousTail;
+  }
+
+  const vector = {
+    x: newHead.x - previousTail.x,
+    y: newHead.y - previousTail.y,
+  };
+
+  // move left
+  if (vector.x < 0 && vector.y === 0)
+    return calculateNextPoint('L', previousTail);
+  // move right
+  if (vector.x > 0 && vector.y === 0)
+    return calculateNextPoint('R', previousTail);
+
+  // move up
+  if (vector.x === 0 && vector.y > 0)
+    return calculateNextPoint('U', previousTail);
+  // move down
+  if (vector.x === 0 && vector.y < 0)
+    return calculateNextPoint('D', previousTail);
+
+  // move left up
+  if (vector.x < 0 && vector.y > 0)
+    return calculateNextPoint('LU', previousTail);
+  // move left down
+  if (vector.x < 0 && vector.y < 0)
+    return calculateNextPoint('LD', previousTail);
+
+  // move right up
+  if (vector.x > 0 && vector.y > 0)
+    return calculateNextPoint('RU', previousTail);
+  // move right down
+  if (vector.x > 0 && vector.y < 0)
+    return calculateNextPoint('RD', previousTail);
+
+  console.log('distance: ', distance);
+
   return previousTail;
 };
 
@@ -61,11 +116,11 @@ const visualizeMap = (game: GameHistory) => {
   const currentPoint = {
     head: {
       ...gamePoint.head,
-      y: - gamePoint.head.y + height - 1,
+      y: -gamePoint.head.y + height - 1,
     },
     tail: {
       ...gamePoint.tail,
-      y: - gamePoint.tail.y + height - 1,
+      y: -gamePoint.tail.y + height - 1,
     },
   };
 
@@ -86,7 +141,7 @@ const visualizeMap = (game: GameHistory) => {
 const makeMove = (move: Move) => {
   const state = GAME_MAP.at(-1)!;
 
-  const newHead = calculateNextPoint(move, state.head);
+  const newHead = calculateNextPoint(move.direction, state.head);
   const newTail = calculateTailPosition(state.tail, newHead);
 
   GAME_MAP.push({
@@ -109,6 +164,18 @@ const visualizeInitialStep = (initial: GameHistory) => {
   visualizeMap(initial);
 };
 
+const calculateTailVisits = (game: GameHistory): number => {
+  const uniquePoints = [
+    ...new Map(
+      game.map(state => state.tail)
+      .map(point => [JSON.stringify(point), point])
+    )
+    .values()
+  ]
+
+  return uniquePoints.length;
+}
+
 export function runPartOne(input: string): number {
   const commands = input.split('\n').map((line) => {
     const [direction, steps] = line.split(' ');
@@ -120,9 +187,9 @@ export function runPartOne(input: string): number {
 
   visualizeInitialStep(GAME_MAP);
 
-  [commands[0], commands[1]].forEach(executeCommand);
+  commands.forEach(executeCommand);
 
-  return 1;
+  return calculateTailVisits(GAME_MAP);
 }
 
 export function runPartTwo(input: string): number {
