e8c63f2cf9c5b5bacc2f01dd4d1993385fd8b870
Michal Michalczuk <michalczukm@gmail.com>
2022-12-13 15:55:01 +0100

    day 12 solved

--- day-12/index.ts
+++ day-12/index.ts
@@ -5,6 +5,7 @@ type Point = {
   numericValue: number;
   isStart: boolean;
   isEnd: boolean;
+  minSteps: number;
 };
 
 const is = <T>(item: T | undefined): item is T => item !== undefined;
@@ -17,17 +18,28 @@ const generatePath = (
   pointsMap: Map<string, Point>,
   startingPoint: Point,
   currentPath: Point[],
-  permutations: Point[][]
-): Point[][] => {
+  permutations: Point[][],
+  min: number
+): number => {
   currentPath.push(startingPoint);
+  const steps = currentPath.length;
 
-  if(currentPath.length >= 50) {
-    return permutations;
+  if (startingPoint.minSteps > steps) {
+    startingPoint.minSteps = steps;
+  } else {
+    return min;
   }
 
   if (startingPoint.isEnd) {
     permutations.push(currentPath);
-    return permutations;
+    if (min > steps) {
+      min = steps;
+    }
+    return min;
+  }
+
+  if (min < steps) {
+    return min;
   }
 
   const neighborhoods = [
@@ -42,38 +54,52 @@ const generatePath = (
   ]
     .map((p) => pointsMap.get(pointToKey(p)))
     .filter(is)
-    .filter((p) => p.x >= 0 && p.y >= 0 && !currentPath.includes(p));
+    .filter((p) => !currentPath.includes(p));
 
   const options = neighborhoods
     .map(pointToKey)
     .map((key) => pointsMap.get(key))
     .filter(is)
-    .filter((p) => p.numericValue - startingPoint.numericValue <= 1);
+    .filter((p) => startingPoint.numericValue - p.numericValue <= 1);
 
-  return options.flatMap((point) =>
-    generatePath(pointsMap, point, [...currentPath], permutations)
+  return Math.min(
+    ...options.map((point) =>
+      generatePath(pointsMap, point, [...currentPath], permutations, min)
+    )
   );
 };
 
 export function runPartOne(input: string): number {
   const points: Point[] = input.split('\n').flatMap((line, rowNo) =>
-    line.split('').filter(item => item !== ' ').map((item, colNo) => {
-      const value = item === 'S' ? 'a' : item === 'E' ? 'z' : item;
-      return {
-        x: rowNo,
-        y: colNo,
-        value,
-        numericValue: value.charCodeAt(0) - 97,
-        isStart: item === 'S',
-        isEnd: item === 'E',
-      };
-    })
+    line
+      .split('')
+      .filter((item) => item !== ' ')
+      .map((item, colNo) => {
+        const value = item === 'S' ? 'a' : item === 'E' ? 'z' : item;
+        return {
+          x: rowNo,
+          y: colNo,
+          value,
+          numericValue: value.charCodeAt(0) - 97,
+          isStart: item === 'E',
+          isEnd: item === 'S',
+          minSteps: Infinity,
+        };
+      })
   );
 
+  console.time('run part one;');
   const pointsMap = new Map(points.map((point) => [pointToKey(point), point]));
-  const paths = generatePath(pointsMap, points.find((p) => p.isStart)!, [], []);
+  const min = generatePath(
+    pointsMap,
+    points.find((p) => p.isStart)!,
+    [],
+    [],
+    Infinity
+  );
 
-  return Math.min(...paths.map((p) => p.length - 1));
+  console.timeEnd('run part one;');
+  return min - 1;
 }
 
 export function runPartTwo(input: string): number {
